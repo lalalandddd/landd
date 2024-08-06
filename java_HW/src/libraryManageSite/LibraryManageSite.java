ecc352e221b33c6e9e544c5b437c02b91afababb
landd <ry0606@naver.com>
2024-08-06 09:18:02 +0900

    숙제와 중간프로젝트

---
+++ java_HW/src/libraryManageSite/LibraryManageSite.java
@@ -0,0 +1,15 @@
+package libraryManageSite;
+
+public class LibraryManageSite {
+
+	public static void main(String[] args) {
+/*
+-- 도서 관리 사이트(도서 등록, 수정, 삭제, 검색) --
+	총 페이지(jsp) 수는 5~10페이지
+	책 클래스 필수 값 : 제목, 페이지수, 지은이, 출판사, 코드번호, 분류, 목차
+	검색 : 분류, 제목, 지은이, 출판사 별로... 혹은 한번에 통합 검색
+	책은 30권 이상 등록
+	※ java의 마지막 수업인 (게시판 만들기)가 가능하다면 모두 가능하다.
+*/
+	}
+}
